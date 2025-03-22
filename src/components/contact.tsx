1267ee4574bee7b6a58364c090109a6d5b95d4b5
paul8478 <somnathpaul626@gmail.com>
2025-03-22 23:21:19 +0530

    contact in front page is fixed

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -18,24 +18,20 @@ export const Contact = () => {
       <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
         <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
           <ImageClipBox
-            src="/img/contact-1.webp"
+            src="/img/art4.jpg"
             alt="Contact bg 1"
             clipClass="contact-clip-path-1"
           />
 
-          <ImageClipBox
-            src="/img/contact-2.webp"
+          {/* <ImageClipBox
+            src="/img/art3.webp"
             alt="Contact bg 2"
             clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
-          />
+          /> */}
         </div>
 
         <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
-          <ImageClipBox
-            src="/img/swordman-partial.webp"
-            alt="Swordman partial"
-            clipClass="absolute md:scale-125"
-          />
+          
 
           <ImageClipBox
             src="/img/swordman.webp"
@@ -45,14 +41,14 @@ export const Contact = () => {
         </div>
 
         <div className="flex flex-col items-center text-center">
-          <p className="font-general text-[10px] uppercase">Join Zentry</p>
+          <p className="font-general text-[10px] uppercase">Reach Us</p>
 
           <p className="special-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]">
-            Let&apos;s b<b>u</b>ild the
-            <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether
+            Having Any
+            <br /> kind of Trouble ? <br /> <span className="text-sky-400">Contact Us</span>
           </p>
 
-          <Button containerClass="mt-10 cursor-pointer">Contact Us</Button>
+          <a href="contact"><Button containerClass="mt-10 cursor-pointer">Contact Us</Button></a>
         </div>
       </div>
     </section>
